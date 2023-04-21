5b0ff4709c93e234af2256ffdefbd055c0e48d7c
farzin <farzinsharif.j@gmail.com>
2023-04-21 14:19:21 +0430

    fix bug ready to use

--- webqs.py
+++ webqs.py
@@ -2,8 +2,9 @@ import requests as req
 import json
 import re
 from config_webqs import *
-
+import config_webqs as conf
 def data_capture():
+    global json_data
     response = req.get(URL_API).text
     json_data = json.loads(response)
     return json_data
@@ -14,15 +15,16 @@ def data_capture():
 #  f.write(str(json_data))
 #jsut run it once so you have backup
 
-def uni_name(json_data):
+def uni_name():
+
     for i in json_data["data"]:
-        if json_data["data"][counter]["country"] == country_name:
-            needed_data = json_data["data"][counter]["title"]
+        if json_data["data"][conf.counter]["country"] == country_name:
+            needed_data = json_data["data"][conf.counter]["title"]
             link_list.append(needed_data)
-            university_counter += 1
-        counter += 1
+            conf.university_counter += 1
+        conf.counter += 1
     university_name_list=re.findall(r'>+\w+.*?<',str(link_list))
-    print(university_name_list,'\n',f"Universities count availble for {country_name} is :",university_counter)
+    print(university_name_list,'\n',f"Universities count availble for {country_name} is :",conf.university_counter)
 
 if __name__ == '__main__' :
     data_capture()
